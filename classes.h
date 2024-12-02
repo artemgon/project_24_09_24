7f6da3161e8cc897dea692d0a34e5fbb4ffb2050
artemgon <artemhoncharuk123@gmail.com>
2024-12-02 14:18:36 +0200

    Task 2 is completed. HW is executed.

--- classes.h
+++ classes.h
@@ -1,1 +1,31 @@
 #include "libs.h"
+
+class Complex
+{
+private:
+	float re, im;
+public:
+	Complex(float r_p, float i_p) : re(r_p), im(i_p) {}
+	Complex operator+(const Complex& n_1)
+	{
+		return Complex(re + n_1.re, im + n_1.im);
+	}
+	Complex operator-(const Complex& n_1)
+	{
+		return Complex(re - n_1.re, im - n_1.im);
+	}
+	Complex operator*(const Complex& n_1)
+	{
+		return Complex((re * n_1.re) - (im * n_1.im), (re * n_1.im) + (im * n_1.re));
+	}
+	Complex operator/(const Complex& n_1)
+	{
+		float denominator = n_1.re * n_1.re + n_1.im * n_1.im;
+		return Complex((re * n_1.re + im * n_1.im) / denominator,
+			(im * n_1.re - re * n_1.im) / denominator);
+	}
+	void printFunc() const
+	{
+		cout << re << " + " << im << " * i" << endl;
+	}
+};
--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 
 int main()
 {
+	Complex c_n_1(3, 4), c_n_2(1, 2);
+	cout << "Sum: ";
+	Complex sum = c_n_1 + c_n_2;
+	sum.printFunc();
+	cout << "Difference: ";
+	Complex difference = c_n_1 - c_n_2;
+	difference.printFunc();
+	cout << "Product: ";
+	Complex product = c_n_1 * c_n_2;
+	product.printFunc();
+	cout << "Quotient: ";
+	Complex quotient = c_n_1 / c_n_2;
+	quotient.printFunc();
 	return 0;
 }
